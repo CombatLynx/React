30fd60787adb36564143d03a7c0ed267f66a2be5
lynx <snef7@yandex.ru>
2022-11-21 13:14:19 +0300

    creating a technology component and header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Header = () => {
+    return (
+        <div className="header">Header</div>
+    );
+}
+
+const Technology = () => {
+    return (
+        <div className="technology">Technology</div>
+    );
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Header/>
+          <Technology/>
+      </div>
   );
 }
 

940829ecc752012179e33c236d3e857606bebb2e
CombatLynx <snef7@yandex.ru>
2022-11-30 18:17:50 +0300

    detail dialogs cpmponent

--- my-app/src/components/Dialogs/Dialogs.js
+++ my-app/src/components/Dialogs/Dialogs.js
@@ -1,9 +1,36 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
-const Dialogs = () => {
+const setActive = ({ isActive }) => (isActive ? classes.active: '');
+
+const Dialogs = (props) => {
     return (
-      <div>Dialogs</div>
+      <div className={classes.dialogs}>
+          <div className={classes["dialogs-items"]}>
+              <div className={classes["dialogs-items__element"]}>
+                  <NavLink className={setActive} to="/dialogs/1">Misha1</NavLink>
+              </div>
+              <div className={classes["dialogs-items__element"]}>
+                  <NavLink className={setActive} to="/dialogs/2">Misha2</NavLink>
+              </div>
+              <div className={classes["dialogs-items__element"]}>
+                  <NavLink className={setActive} to="/dialogs/3">Misha3</NavLink>
+              </div>
+              <div className={classes["dialogs-items__element"]}>
+                  <NavLink className={setActive} to="/dialogs/4">Misha4</NavLink>
+              </div>
+              <div className={classes["dialogs-items__element"]}>
+                  <NavLink className={setActive} to="/dialogs/5">Misha5</NavLink>
+              </div>
+          </div>
+          <div className={classes["dialogs-messages"]}>
+              <NavLink to="/" className={classes["dialogs-messages__element"]}>6</NavLink>
+              <NavLink to="/" className={classes["dialogs-messages__element"]}>7</NavLink>
+              <NavLink to="/" className={classes["dialogs-messages__element"]}>8</NavLink>
+              <NavLink to="/" className={classes["dialogs-messages__element"]}>9</NavLink>
+          </div>
+      </div>
     );
 }
 
--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -2,15 +2,17 @@ import React from "react";
 import classes from "./Navbar.module.css";
 import {NavLink} from "react-router-dom";
 
+const setActive = ({ isActive }) => (isActive ? classes.active: '');
+
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <ul className={classes["nav-menu"]}>
-                <li className={classes["nav-menu__item"]}><NavLink className={({ isActive }) => (isActive ? classes.active: '')} to="/profile">Profile</NavLink></li>
-                <li className={classes["nav-menu__item"]}><NavLink className={({ isActive }) => (isActive ? classes.active: '')} to="/dialogs">Messages</NavLink></li>
-                <li className={classes["nav-menu__item"]}><NavLink className={({ isActive }) => (isActive ? classes.active: '')} to="/news">News</NavLink></li>
-                <li className={classes["nav-menu__item"]}><NavLink className={({ isActive }) => (isActive ? classes.active: '')} to="/music">Music</NavLink></li>
-                <li className={classes["nav-menu__item"]}><NavLink className={({ isActive }) => (isActive ? classes.active: '')} to="/settings">Settings</NavLink></li>
+                <li className={classes["nav-menu__item"]}><NavLink className={setActive} to="/profile">Profile</NavLink></li>
+                <li className={classes["nav-menu__item"]}><NavLink className={setActive} to="/dialogs">Messages</NavLink></li>
+                <li className={classes["nav-menu__item"]}><NavLink className={setActive} to="/news">News</NavLink></li>
+                <li className={classes["nav-menu__item"]}><NavLink className={setActive} to="/music">Music</NavLink></li>
+                <li className={classes["nav-menu__item"]}><NavLink className={setActive} to="/settings">Settings</NavLink></li>
             </ul>
         </nav>
     );

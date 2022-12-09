8b8a7850892243d222ce91a1ae7f5c22a2d33468
lynx <snef7@yandex.ru>
2022-12-09 13:07:37 +0300

    create store

--- my-app/src/redux/state.js
+++ my-app/src/redux/state.js
@@ -1,91 +1,92 @@
-let rerenderEntireTree = () => {
-    console.log('rerender DOM');
-}
-
-let state = {
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                message: "Hi, how are you?",
-                countLikes: 12
-            },
-            {
-                id: 2,
-                message: "It is my post",
-                countLikes: 7
-            }
-        ],
-        newTextPost: "default message"
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    message: "Hi, how are you?",
+                    countLikes: 12
+                },
+                {
+                    id: 2,
+                    message: "It is my post",
+                    countLikes: 7
+                }
+            ],
+            newTextPost: "default message"
+        },
+        messagesPage: {
+            dialogs: [
+                {
+                    id: "1",
+                    name: "Doe"
+                },
+                {
+                    id: "2",
+                    name: "Smith"
+                },
+                {
+                    id: "3",
+                    name: "Jones"
+                }
+            ],
+            messages: [
+                {
+                    id: "1",
+                    message: "Hi, i am chicha"
+                },
+                {
+                    id: "2",
+                    message: "Second massage"
+                },
+                {
+                    id: "3",
+                    message: "npx cfcr Message"
+                }
+            ]
+        },
+        sidebar: {
+            friends: [
+                {
+                    id: 1,
+                    name: "Petya"
+                },
+                {
+                    id: 2,
+                    name: "Vasya"
+                },
+                {
+                    id: 3,
+                    name: "Kolya"
+                }
+            ]
+        }
     },
-    messagesPage: {
-        dialogs: [
-            {
-                id: "1",
-                name: "Doe"
-            },
-            {
-                id: "2",
-                name: "Smith"
-            },
-            {
-                id: "3",
-                name: "Jones"
-            }
-        ],
-        messages: [
-            {
-                id: "1",
-                message: "Hi, i am chicha"
-            },
-            {
-                id: "2",
-                message: "Second massage"
-            },
-            {
-                id: "3",
-                message: "npx cfcr Message"
-            }
-        ]
+    _callSubscriber() {
+        console.log('rerender DOM');
     },
-    sidebar: {
-        friends: [
-            {
-                id: 1,
-                name: "Petya"
-            },
-            {
-                id: 2,
-                name: "Vasya"
-            },
-            {
-                id: 3,
-                name: "Kolya"
-            }
-        ]
+    getState() {
+        return this._state;
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.profilePage.newTextPost,
+            countLikes: 0
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newTextPost = '';
+        this._callSubscriber(this._state);
+    },
+    updatePostText(newText) {
+        this._state.profilePage.newTextPost = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 }
 
-window.state = state;
+window.store = store;
 
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.profilePage.newTextPost,
-        countLikes: 0
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newTextPost = '';
-    rerenderEntireTree(state);
-}
-
-export const updatePostText = (newText) => {
-    state.profilePage.newTextPost = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;

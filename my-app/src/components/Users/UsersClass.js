70970cf1e9511a05058263690d98b1a9c72a6159
CombatLynx <snef7@yandex.ru>
2022-12-30 15:44:37 +0300

    componentDidMount, life cycle methods

--- my-app/src/components/Users/UsersClass.js
+++ my-app/src/components/Users/UsersClass.js
@@ -7,7 +7,9 @@ class UsersClass extends React.Component {
 
     constructor(props) {
         super(props);
+    }
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);

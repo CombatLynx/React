3483e6c534b87e50158f583eb2c304144f1451b3
lynx <snef7@yandex.ru>
2023-01-11 16:49:47 +0300

    private repository

--- my-app/src/components/Header/HeaderContainer.js
+++ my-app/src/components/Header/HeaderContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import {connect} from "react-redux";
-import {getAuthThunkCreator, setAuthThunkCreator} from "../../redux/auth-reducer";
+import {getAuthThunkCreator} from "../../redux/auth-reducer";
 
 class HeaderContainer extends React.Component {
 

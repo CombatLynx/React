2756ccd0688a446804e1ab126f6c08c680664307
lynx <snef7@yandex.ru>
2023-01-18 10:30:32 +0300

    revert previous commit changes: MyPosts transformation in classComponent and add localState

--- my-app/src/components/Profile/MyPosts/MyPosts.js
+++ my-app/src/components/Profile/MyPosts/MyPosts.js
@@ -2,57 +2,38 @@ import React from "react";
 import Post from "./Post";
 import classes from "./MyPosts.module.css";
 
-class MyPosts extends React.Component {
-    constructor(props) {
-        super(props);
+const MyPosts = (props) => {
 
-        this.state = {
-            textPost: this.props.textPost
+    let dataPost = props.dataPost.map(
+        (dataElement) => {
+            return <Post message={dataElement.message} countLikes={dataElement.countLikes}></Post>
         }
+    )
+
+    let onAddPost = () => {
+        props.addPost();
     }
 
-    onAddPost = () => {
-        this.props.updatePostText(this.state.textPost);
-        this.props.addPost(this.state.textPost);
+    let onChangeTextPost = (e) => {
+        let textPost = e.target.value;
+        props.updatePostText(textPost);
     }
 
-    onChangeTextPost = (e) => {
-        this.setState({
-            textPost: e.currentTarget.value
-        })
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.textPost !== this.props.textPost) {
-            this.setState({
-                textPost: this.props.textPost
-            })
-        }
-    }
-
-    render() {
-        return (
-            <div className={classes.posts}>
-                <div className={classes["posts-add"]}>My posts
-                    <div className={classes["posts-textarea"]}>
-                    <textarea onChange={this.onChangeTextPost}
-                              value={this.state.newTextPost}>
+    return (
+        <div className={classes.posts}>
+            <div className={classes["posts-add"]}>My posts
+                <div className={classes["posts-textarea"]}>
+                    <textarea onChange={onChangeTextPost}
+                              value={props.newTextPost}>
                     </textarea>
-                    </div>
-                    <div>
-                        <button onClick={this.onAddPost}>Add post</button>
-                    </div>
                 </div>
-                {
-                    this.props.dataPost.map(
-                        (dataElement) => {
-                            return <Post message={dataElement.message} countLikes={dataElement.countLikes}></Post>
-                        }
-                    )
-                }
+                <div>
+                    <button onClick={onAddPost}>Add post</button>
+                </div>
             </div>
-        );
-    }
+            {dataPost}
+        </div>
+    );
 }
 
 export default MyPosts;

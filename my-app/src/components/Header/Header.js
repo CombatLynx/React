165b15f5e364a3901b7acfe124e4b05e8ab32144
lynx <snef7@yandex.ru>
2023-02-07 11:36:28 +0300

    create TimerForStaying

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,20 +1,26 @@
 import React from "react";
 import classes from "./Header.module.css";
 import {NavLink} from "react-router-dom";
+import TimerForStaying from "./TimerForStaying/TimerForStaying";
 
 const Header = (props) => {
     return (
         <header className={classes.header}>
-            <img
-                src="https://img2.freepng.ru/20180402/hze/kisspng-service-mark-symbol-registered-trademark-symbol-service-5ac256c44ba545.3203033915226856363099.jpg"
-                alt="logo"/>
-            <div className={classes["login-block"]}>
+            <div className={classes["header-item"]}>
+                <TimerForStaying/>
+            </div>
+            <div className={classes["header-item"]}>
+                <img
+                    src="https://img2.freepng.ru/20180402/hze/kisspng-service-mark-symbol-registered-trademark-symbol-service-5ac256c44ba545.3203033915226856363099.jpg"
+                    alt="logo"/>
+            </div>
+            <div className={classes["login-block"] + classes["header-item"]}>
                 <div>{props.isAuth
                     ? <div>{props.login + " " + props.userId}
                         <div>
                             <button onClick={props.logOut}>Logout</button>
                         </div>
-                      </div>
+                    </div>
                     : <NavLink to={'/login'}>Login</NavLink>}
                 </div>
             </div>
---
+++ my-app/src/components/Header/TimerForStaying/TimerForStaying.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+import classes from "../TimerForStaying/TimerForStaying.module.css";
+
+const TimerForStaying = () => {
+
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+
+    useEffect(() => {
+        if (seconds < 60) {
+            const timer = setInterval(() => {
+                setSeconds(seconds + 1);
+            }, 1000);
+            return () => {
+                clearInterval(timer);
+            }
+        } else {
+            if (minutes < 60) {
+                setMinutes(minutes + 1);
+            } else {
+                setMinutes(0);
+                setHours(hours + 1);
+            }
+            setSeconds(0);
+        }
+    }, [seconds, minutes, hours]);
+
+    return (
+        <div className={classes.timer}>
+            <div>Время прибывания на сайте</div>
+            <div>
+                {seconds}<span>:sec</span>
+                {minutes}<span>:min</span>
+                {hours}<span>:hours</span>
+            </div>
+        </div>
+    );
+}
+
+export default TimerForStaying;

7f2a458a2840086bd99c4c3db2f03c4544a42e54
lynx <snef7@yandex.ru>
2023-06-06 10:34:10 +0300

    ProfileContainer rewriting into classes

---
+++ my-app/src/components/Profile/ProfileContainer.js
@@ -0,0 +1,86 @@
+import React from "react";
+import {connect} from "react-redux";
+import {useLocation, useParams} from "react-router-dom";
+import {
+    getProfileStatusThunkCreator,
+    getProfileThunkCreator,
+    savePhotoProfileThunkCreator,
+    saveProfileInfoThunkCreator,
+    updateStatusThunkCreator
+} from "../../redux/profile-reducer";
+import Profile from "./Profile";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {compose} from "redux";
+
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{location, params}}
+            />
+        );
+    }
+
+    return ComponentWithRouterProp;
+}
+
+class ProfileContainer extends React.Component {
+    refreshProfile() {
+        if (!this.props.router.params.userId) {
+            this.props.router.params.userId = this.props.authorizedUserId;
+            if (!this.props.authorizedUserId) {
+                this.props.history.push('/login');
+            }
+        }
+
+        this.props.getProfile(this.props.router.params.userId);
+        this.props.getStatusProfile(this.props.router.params.userId);
+    }
+
+    componentDidMount() {
+        this.refreshProfile();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.router.params.userId !== prevProps.router.params.userId) {
+            this.refreshProfile();
+        }
+    }
+
+    render() {
+        return (
+            <Profile {...this.props}
+                     authorizedUserId={this.props.authorizedUserId}
+                     isOwner={this.props.router.params.userId}
+                     status={this.props.status}
+                     profile={this.props.profile}
+                     updateStatusProfile={this.props.updateStatusProfile}
+                     savePhotoProfile={this.props.savePhotoProfile}
+                     saveProfileInfo={this.props.saveProfileInfo}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        profile: state.profilePage.profile,
+        status: state.profilePage.status,
+        authorizedUserId: state.auth.userId
+    }
+}
+
+export default compose(
+    connect(mapStateToProps, {
+        getProfile: getProfileThunkCreator,
+        getStatusProfile: getProfileStatusThunkCreator,
+        updateStatusProfile: updateStatusThunkCreator,
+        savePhotoProfile: savePhotoProfileThunkCreator,
+        saveProfileInfo: saveProfileInfoThunkCreator
+    }),
+    withRouter,
+    withAuthRedirect
+)(ProfileContainer);
